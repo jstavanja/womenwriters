d2e9fd6dabecfd7b7cb6e6662582e87ed33f8f59
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2018-05-05 15:19:03 +0200

    initial vueJS project structure :tada:

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="header">
+    header navigation will be
+    <div class="notr">
+      Here
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+  nav.header {
+    font-size: 20px;
+    .notr {
+      font-size: 50px;
+    }
+  }
+</style>
---
+++ src/components/Visualization.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="visualization">
+    the visualization will be here.<br>
+    this component will take in data about what to fetch from the original website<br>
+    and then pass it on to the drawing libraries and so on.
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+  section.visualization {
+    color: lightcoral;
+  }
+</style>

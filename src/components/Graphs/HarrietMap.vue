7174c676169f3ba2d960b67b1acf67d0a30c9b1e
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2018-06-10 16:44:15 +0200

    Added reloading of map if google object doesn't initialize

--- src/components/Graphs/HarrietMap.vue
+++ src/components/Graphs/HarrietMap.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="map-harriet">
+    <el-row v-if="loadErr">
+      Map cannot be loaded. Please click this button to refresh: <el-button @click="tryDraw">Refresh</el-button>
+    </el-row>
     <el-row class="graph-container animated fadeIn fd1">
 		  <div id="map"></div>
     </el-row>
@@ -10,6 +13,11 @@
 import anglophoneReceptions from '../../datasets/anglophone_receptions.json';
 
 export default {
+  data () {
+    return {
+      loadErr: false
+    }
+  },
   mounted () {
 		let recaptchaScript = document.createElement('script')
     recaptchaScript.setAttribute('src', 'https://www.gstatic.com/charts/loader.js')
@@ -18,11 +26,7 @@ export default {
   created () {
     const dataHeader = ['Country', 'Receptions']
     this.countriesArray = [dataHeader, ...Object.entries(anglophoneReceptions["Harriet Elisabeth Beecher Stowe"]).map((entry) => [entry[1].country, entry[1].receptions])]
-    google.charts.load('current', {
-      'packages':['geochart'],
-      'mapsApiKey': 'AIzaSyAWMQ35Ki6bgjtOQ8DXxKOthQATcfnVVd0'
-    });
-    google.charts.setOnLoadCallback(this.drawRegionsMap);
+    this.tryDraw()
   },
   methods: {
     drawRegionsMap () {
@@ -35,6 +39,18 @@ export default {
 
       let chart = new google.visualization.GeoChart(document.getElementById('map'));
       chart.draw(data, options);
+    },
+    tryDraw () {
+      if (window.google) {
+        google.charts.load('current', {
+          'packages':['geochart'],
+          'mapsApiKey': 'AIzaSyAWMQ35Ki6bgjtOQ8DXxKOthQATcfnVVd0'
+        })
+        google.charts.setOnLoadCallback(this.drawRegionsMap)
+        this.loadErr = false
+      } else {
+        this.loadErr = true
+      }
     }
   }
 }

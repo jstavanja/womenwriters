8cf03cab26deb0c406fb5365c348edd16e6e39f7
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2018-06-10 17:58:59 +0200

    Fixed query for which countries exported

--- src/components/Questions/WhichCountriesExported.vue
+++ src/components/Questions/WhichCountriesExported.vue
@@ -39,16 +39,50 @@ export default {
     document.head.appendChild(recaptchaScript)
 	},
   created () {
-    axios.post('https://repository.huygens.knaw.nl/solr/wwdocumentreceptions/select',
-      'q=*:*&fq=authorGender_ss%3A(%22FEMALE%22)&fq=publishLocation_ss%3A(%22Norway%22)&fq=type_s%3Adocument_reception&facet.field=relationType_s&facet.field=authorName_ss&facet.field=authorGender_ss&facet.field=date_i&facet.field=publishLocation_ss&facet.field=language_ss&facet.field=genre_ss&facet.field=source_ss&facet.field=documentType_s&rows=1000&facet.limit=-1&facet.sort=count&&start=0&facet=on&wt=json')
+    axios.post('https://repository.huygens.knaw.nl/solr/wwdocumentreceptions/select', 'q=*:*&fq=type_s%3Adocument_reception&fq=%7B!parent%20which%3Dtype_s%3Adocument_reception%7Dperson_gender_s%3A(%22FEMALE%22)&fq=publishLocation_ss%3A(%22Norway%22)&facet.field=document_authorName_ss&facet.field=publishLocation_ss&rows=27437&facet.limit=-1&facet.sort=count&&start=0&facet=on&wt=json')
       .then((res) => res.data.response.docs)
       .then((works) => {
         let countriesUnique = {}
         works.forEach((work) => {
-          if (work.document_locationSort_s in countriesUnique) {
-            countriesUnique[work.document_locationSort_s]++
-          } else {
-            countriesUnique[work.document_locationSort_s] = 1
+          let location = work.document_locationSort_s
+          let allLocations = [location]
+
+          if (location) {
+            if (location !== 'Norway') {
+              // filter all weird one string combinations
+              if (location.includes('ITA')) {
+                allLocations = ['Italy']
+              } 
+              if (location === 'Scotland' || location === 'Great Britain') {
+                allLocations = ['England']
+              }
+              if (location.includes('CZE')) {
+                allLocations = ['Czech Republic']
+              } 
+              if (location === 'England Germany') {
+                allLocations = ['England', 'Germany']
+              }
+              if (location === 'Denmark Norway') {
+                allLocations = []
+              }
+              if (location === 'England United States') {
+                allLocations = ['England', 'United States']
+              } 
+              if (location === 'Finland Sweden') {
+                allLocations = ['Finland', 'Sweden']
+              }
+              if (location === 'United Kingdom United States') {
+                allLocations = ['England', 'United States']
+              }
+              
+              allLocations.forEach(location => {
+                if (location in countriesUnique) {
+                  countriesUnique[location]++
+                } else {
+                  countriesUnique[location] = 1
+                }
+              })
+            }
           }
         })
 
